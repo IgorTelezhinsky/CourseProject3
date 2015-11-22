877fc9fa48e893b2eec1655ae2a604c6defaf5c0
IgorTelezhinsky <telezhinsky@gmail.com>
2015-11-22 11:04:17 +0100

    Submission ready Course Project

---
+++ README.md
@@ -0,0 +1,80 @@
+---
+title: "README file for the Course Project (Getting and Cleaning Data Class)."
+author: "Igor Telezhinsky"
+date: "2015-11-22"
+---
+
+### Introduction
+
+The purpose of this project is to demonstrate the ability to collect, work with, and clean a data set.
+The goal is to prepare tidy data that can be used for later analysis. The analysis is performed with
+`run_analysis.R` script.  
+
+### Instructions
+
+The raw data is automatically downloaded, unpacked, and analyzed (cleaned) by
+`run_analysis(work_dir,outfile,data_dir)` function within `run_analysis.R` script. To run the function,
+the user is required to provide at least one argument, which is `work_dir` specifying analysis
+working directory. If there is no such directory, it will be created by the script. Optionally,
+the user can provide the output file name for the tidy data  with `outfile` argument and the directory
+name where to download the raw data with `data_dir` argument, both relative to the working directory. 
+
+
+#### Example:
+
+```{r}
+source('./run_analysis.R')
+run_analysis("~/CleanData","tidy_data.txt","RawData")
+```
+or simply
+```{r}
+source('./run_analysis.R')
+run_analysis("~/CleanData")
+```
+
+### Description of the `run_analysis.R` Script
+
+The `run_analysis.R` script contains four functions: 
+
+1. `getRawData(data_dir)` - downloads and unpacks the raw data to `data_dir`;
+
+2. `getStep4DataSet(files)` - prepares data set as it should be after Step 4 of the Course Project Task,
+`files` is a data frame containing paths to the raw data files used for the analysis. The `files` data frame is
+created within `run_analysis()` function;
+
+3. `run_analysis(work_dir,outfile="tidy_data.txt",data_dir="./RawData")` - performs actual analysis in
+working directory specified by `work_dir` argument;
+
+4. `read_tidy(filename="tidy_data.txt")` - reads in tidy data in to R's data frame.
+
+### Analysis steps
+
+The `run_analysis()` function of the `run_analysis.R` script goes through the following steps
+(the function responsible for particular step is specified in brackets):
+
+1. downloads the raw data (`getRawData()`)
+2. unpacks the raw data (`getRawData()`)
+3. prepares the data set as it should be in Step 4 of the Project Task (`getStep4DataSet()`)
+    - reads activity names 
+    - reads raw data variables' names 
+    - reads test data files and introduces descriptive variable names 
+    - reads training data files and introduces descriptive variable names 
+    - gets column indeces of various *mean* and *std* quantities in raw data 
+    - selects *mean* and *std* columns from the raw data sets (test and training) and combines these data into a single data frame 
+    - introduces descriptive activity names to this data frame 
+4. melts Step 4 data set with `id=c("Activity","Subject")` (`run_analysis()`)
+5. casts the molten data set and computes mean for `variables` (`run_analysis()`)
+6. renames the first column to "Activity.Subject" (`run_analysis()`)
+7. writes down the tidy data set (`run_analysis()`).
+
+### Auxiliary functions
+
+For convenience, the `run_analysis.R` script includes `read_tidy(filename="tidy_data.txt")` function that reads the tidy data set back to R's data frame.
+
+#### Example
+
+Reading the tidy data back to R's data frame (given that you're in working directory)
+```{r}
+source('./run_analysis.R')
+tidy<-read_tidy("tidy_data.txt")
+```
---
+++ run_analysis.R
@@ -0,0 +1,115 @@
+#Functions implementing the course project
+#of Getting and Cleaning Data on Coursera.
+
+#this function downloads and unpacks raw data if either is not yet done
+getRawData<-function(dir_name)
+{
+    zip_name<-paste("./",dir_name,".zip",sep = "")
+    #downloading the raw data if the data is absent  
+    if(!file.exists(zip_name))
+    {
+        dataUrl<-"https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+        download.file(dataUrl,destfile = zip_name)
+    }
+    
+    #unzipping the raw data if not yet unzipped
+    if(!file.exists(dir_name))
+    {
+        unzip(zip_name,exdir = dir_name,junkpaths = TRUE)
+    }
+}
+
+#this function prepares data set as it should be after Step 4 of the Course Project Task.  
+getStep4DataSet<-function(files)
+{
+    message("Reading raw data and making transformations to get Step 4 data set...")
+    #reading activity names
+    acts_name<-read.table(data_files$activities_names, header = FALSE, col.names = c("ActCode","ActName"))
+    
+    #reading raw data variables' names
+    data_vars<-read.table(data_files$data_variables, stringsAsFactors = FALSE, header = FALSE, col.names = c("VarColumn","VarName"))
+    
+    #reading test data files and introducing descriptive variable names
+    data_test<-read.table(data_files$data_test,         stringsAsFactors = FALSE, header = FALSE, col.names = data_vars$VarName)
+    acts_test<-read.table(data_files$activities_test,   stringsAsFactors = FALSE, header = FALSE, col.names = "Activity")
+    subj_test<-read.table(data_files$subject_test,      stringsAsFactors = FALSE, header = FALSE, col.names = "Subject")
+    
+    #reading training data files and introducing descriptive variable names
+    data_train<-read.table(data_files$data_train,       stringsAsFactors = FALSE, header = FALSE, col.names = data_vars$VarName)
+    acts_train<-read.table(data_files$activities_train, stringsAsFactors = FALSE, header = FALSE, col.names = "Activity")
+    subj_train<-read.table(data_files$subject_train,    stringsAsFactors = FALSE, header = FALSE, col.names = "Subject")
+    
+    #getting column indeces of various mean quantities in data
+    mean_cols<-grep("mean",data_vars$VarName)
+    
+    #getting column indeces of various standard deviation quantities in data
+    std_cols <-grep("std", data_vars$VarName)
+    
+    #selecting mean and std quantities and combining data into single data frame 
+    test_mean_std    <- cbind(data_test[mean_cols], data_test[std_cols], acts_test, subj_test)
+    train_mean_std   <- cbind(data_train[mean_cols],data_train[std_cols],acts_train,subj_train)
+    mean_std         <- rbind(test_mean_std,train_mean_std)
+    
+    #introducing descriptive activity names
+    mean_std$Activity<-acts_name$ActName[match(mean_std$Activity,acts_name$ActCode)]
+    
+    #setting Subject variable as factor
+    mean_std$Subject<-as.factor(mean_std$Subject)
+    
+    message("done!")
+    #returning Step 4 data set
+    mean_std
+}
+
+#this function performs actual analysis
+run_analysis<-function(work_dir,outfile="tidy_data.txt",data_dir="./RawData")
+{
+    #check if working directory exists and if not create one
+    if(!file.exists(work_dir))
+    {
+        dir.create(work_dir)
+    }
+    
+    #setting working directory
+    setwd(work_dir)
+    
+    #getting raw data from web
+    getRawData(data_dir)
+    
+    #placing raw data files used in the analysis to data frame
+    data_files<-data.frame(activities_names =  paste(data_dir,"/activity_labels.txt",sep = ""),
+                           data_variables   =  paste(data_dir,"/features.txt",sep = ""),
+                           data_test        =  paste(data_dir,"/X_test.txt",sep = ""),
+                           activities_test  =  paste(data_dir,"/y_test.txt",sep = ""),
+                           subject_test     =  paste(data_dir,"/subject_test.txt",sep = ""),
+                           data_train       =  paste(data_dir,"/X_train.txt",sep = ""),
+                           activities_train =  paste(data_dir,"/y_train.txt",sep = ""),
+                           subject_train    =  paste(data_dir,"/subject_train.txt",sep = ""),
+                           stringsAsFactors = FALSE)
+    
+    #getting Step 4 data set
+    prepared_data <- getStep4DataSet(data_files)
+    
+    message("Preparing tidy data set from Step 4 data set and writing it to file...")
+    
+    #melting dataset
+    m<-melt(prepared_data,id=c("Activity","Subject"))
+    
+    #casting molten data set and computing mean for variables
+    d<-dcast(m,interaction(Subject,Activity)~variable,mean)
+    
+    #renaming first column 
+    colnames(d)[1]<-"Subject.Activity"
+    
+    #writing to file
+    write.table(d,outfile,row.name = FALSE)
+    
+    message("done!") 
+}
+
+#reads tidy data
+read_tidy<-function(filename="tidy_data.txt")
+{
+    r<-read.table("tidy_data.txt",header = TRUE)
+    r
+}
